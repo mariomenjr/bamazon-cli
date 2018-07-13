0a2fa6df7eb3ce73916f5fb4a0e712dace25481a
Mario Menjivar <mariomenjr@gmail.com>
2018-07-13 13:05:00 -0700

    Added inquirer to Store.js

--- store.js
+++ store.js
@@ -1,10 +1,43 @@
 const Table = require("cli-table");
-
 const Conn = require("./conn");
+
+const inquirer = require("inquirer");
 
 function Store() {
   this.conn = new Conn();
 }
+
+Store.prototype.sell = function() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the item# you'd like to purchase:",
+        name: "id",
+        validate: (input, hash) => {
+          return Number(input)>0;
+        }
+      },
+      {
+        type: "input",
+        message: "How many units would you like to purchase:",
+        name: "sale_qty",
+        validate: (input, hash) => {
+          return Number(input)>0;
+        }
+      }
+    ])
+    .then(answers => {
+      this.checkInventory(answers);
+    })
+    .catch(err => {
+      throw err;
+    });
+};
+
+Store.prototype.checkInventory = function(answers) {
+  console.log("Checking Inventory", answers);
+};
 
 Store.prototype.showAll = function() {
   this.conn.connect();
@@ -27,6 +60,7 @@ Store.prototype.cliPrint = function(obj) {
     table.push([el.id, el.product_name, el.dept_fk, el.price, el.stock_qty]);
   }
   console.log(table.toString());
+  this.sell();
 };
 
 module.exports = Store;
